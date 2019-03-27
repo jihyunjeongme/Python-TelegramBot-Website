99fd4985305b840e8ab39c9e69f5aed59e4542bd
jihyunjeongme <jihyunjeong.me@gmail.com>
2019-03-27 19:17:06 +0900

    app.py coding

--- app.py
+++ app.py
@@ -20,10 +20,23 @@ req.encoding = "utf-8"  # Clien에서 encoding 정보를 보내주지 않아 enc
 html = req.text
 soup = BeautifulSoup(html, "html.parser")
 posts = soup.select("strong.tit")
+latest = posts[1].text
 
-for i, v in enumerate(posts):
-    if (len(posts) - 1) == i:
-        pass
-    else:
-        print(v.text.strip())
+with open(os.path.join(BASE_DIR, "latest.txt"), "r+") as f_read:
+    before = f_read.readline()
+    if before != latest:
+        # 같은 경우는 에러 없이 넘기고, 다른 경우에만
+        # 메시지 보내는 로직을 넣으면 됩니다.
+        f_read.close()
+
+with open(os.path.join(BASE_DIR, "latest.txt"), "w+") as f_write:
+    f_write.write(latest)
+    f_write.close()
+
+
+# for i, v in enumerate(posts):
+#     if (len(posts) - 1) == i:
+#         pass
+#     else:
+#         print(v.text.strip())
 
